75ac71bc7126b266c852774039e382d3879929a0
Elise Phan <aliceconnic@gmail.com>
2019-04-16 20:05:54 +0000

    Automatically backed up by Learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,18 @@
 // Code SimpleComponent Here
+import React from 'react'
+
+class SimpleComponent extends Component {
+  
+  this.state = {
+    mood: "happy"
+  }
+  
+  render()
+  return (
+    <div>
+    
+    </div>
+    )
+}
+
+export default SimpleComponent
